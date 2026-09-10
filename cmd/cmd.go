4605c283e9396a3e64dceef2d7ea47402376b2b4
Daniel J. Manning <daniel@danieljmanningdev.com>
2026-09-10 15:53:45 +0100

    fix: tab spacing

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -37,7 +37,7 @@ func PrintHelp() {
 	fmt.Println(`djm - Go-first web project tooling
 
 Usage:
-  	djm new <name> [--module <path>]
+	djm new <name> [--module <path>]
 	djm add <feature>
 	djm dev
 	djm check
